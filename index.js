9f46116ecf80e9b8e3e0fed6d003fd18317ad754
Altheric Monarch <anugrahabayu4@gmail.com>
2026-05-26 18:34:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
     if (req.method === 'OPTIONS') return res.sendStatus(204);
     next();
