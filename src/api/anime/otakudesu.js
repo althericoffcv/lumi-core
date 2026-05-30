1537f422588036c26c724c1ec9929f9d2477dc3d
Altheric Monarch <anugrahabayu4@gmail.com>
2026-05-31 01:51:02 +0700

    Update otakudesu.js

--- src/api/anime/otakudesu.js
+++ src/api/anime/otakudesu.js
@@ -2,10 +2,10 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 module.exports = (app) => {
-  app.get("/home/otakudesu", async (req, res) => {
+  app.get("/ongoing/otakudesu", async (req, res) => {
     try {
       const response = await axios.get(
-        "https://otakudesu.cloud/",
+        "https://otakudesu.blog/ongoing-anime/",
         {
           headers: {
             "User-Agent":
@@ -13,19 +13,18 @@ module.exports = (app) => {
             "Accept":
               "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept-Language":
-              "id-ID,id;q=0.9,en-US;q=0.8"
+              "id-ID,id;q=0.9,en-US;q=0.8",
+            "Referer": "https://otakudesu.blog/"
           },
           timeout: 15000
         }
       );
 
       const $ = cheerio.load(response.data);
-
-      const ongoing = [];
-      const completed = [];
+      const result = [];
 
       $(".venz ul li").each((i, el) => {
-        ongoing.push({
+        result.push({
           title: $(el).find(".thumbz h2").text().trim(),
           url: $(el).find(".thumb a").attr("href"),
           thumbnail: $(el).find("img").attr("src"),
@@ -34,27 +33,18 @@ module.exports = (app) => {
         });
       });
 
-      $(".venser ul li").each((i, el) => {
-        completed.push({
-          title: $(el).find("h2").text().trim(),
-          url: $(el).find("a").attr("href"),
-          thumbnail: $(el).find("img").attr("src")
-        });
-      });
-
       res.json({
         status: true,
-        result: {
-          ongoing,
-          completed
-        }
+        total: result.length,
+        result
       });
 
     } catch (err) {
       res.status(500).json({
         status: false,
         error: err.message,
-        code: err.response?.status || null
+        code: err.response?.status || null,
+        url: err.config?.url || null
       });
     }
   });
