e4730d7791a47ab39203a4b24e0d1145a5b60b44
Altheric Monarch <anugrahabayu4@gmail.com>
2026-05-31 01:54:10 +0700

    Update otakudesu.js

--- src/api/anime/otakudesu.js
+++ src/api/anime/otakudesu.js
@@ -1,8 +1,7 @@
 const axios = require("axios");
-const cheerio = require("cheerio");
 
 module.exports = (app) => {
-  app.get("/ongoing/otakudesu", async (req, res) => {
+  app.get("/debug/otakudesu", async (req, res) => {
     try {
       const response = await axios.get(
         "https://otakudesu.blog/ongoing-anime/",
@@ -13,38 +12,26 @@ module.exports = (app) => {
             "Accept":
               "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept-Language":
-              "id-ID,id;q=0.9,en-US;q=0.8",
-            "Referer": "https://otakudesu.blog/"
+              "id-ID,id;q=0.9,en-US;q=0.8"
           },
           timeout: 15000
         }
       );
 
-      const $ = cheerio.load(response.data);
-      const result = [];
-
-      $(".venz ul li").each((i, el) => {
-        result.push({
-          title: $(el).find(".thumbz h2").text().trim(),
-          url: $(el).find(".thumb a").attr("href"),
-          thumbnail: $(el).find("img").attr("src"),
-          episode: $(el).find(".epz").text().trim(),
-          date: $(el).find(".newnime").text().trim()
-        });
-      });
-
       res.json({
         status: true,
-        total: result.length,
-        result
+        code: response.status,
+        html: response.data.substring(0, 3000)
       });
 
     } catch (err) {
-      res.status(500).json({
+      res.json({
         status: false,
-        error: err.message,
         code: err.response?.status || null,
-        url: err.config?.url || null
+        url: err.config?.url || null,
+        server: err.response?.headers?.server || null,
+        cf_ray: err.response?.headers?.["cf-ray"] || null,
+        html: String(err.response?.data || "").substring(0, 3000)
       });
     }
   });
